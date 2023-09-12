b9bdc9b4cae6f10e7bf3194e2ca55f8ad1cc08cf
zhk02 <1728931104@qq.com>
2023-09-12 16:02:17 +0800

    清理代码

--- src/main/java/com/os/apps/fileApp/app/TipWindow.java
+++ src/main/java/com/os/apps/fileApp/app/TipWindow.java
@@ -14,36 +14,36 @@ import java.io.IOException;
 import java.net.URL;
 
 public class TipWindow extends Application {
-   String tipString;
+    String tipString;
 
-   public static void main(String[] args) {
-      launch(args);
-   }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-   public TipWindow(String tipString) {
-      this.tipString = tipString;
-   }
+    public TipWindow(String tipString) {
+        this.tipString = tipString;
+    }
 
-   public void start(Stage primaryStage) throws IOException {
-      URL location = this.getClass().getResource("/com/os/apps/fileApp/fxmls/TipWindow.fxml");
-      if (location == null) {
-         System.out.println("null");
-      } else {
-         FXMLLoader fxmlLoader = new FXMLLoader();
-         fxmlLoader.setLocation(location);
-         Parent root = fxmlLoader.load();
-         primaryStage.setTitle("提示");
-         Scene MainScene = new Scene(root);
-         primaryStage.setScene(MainScene);
-         Scene scene = primaryStage.getScene();
-         TipWindowCtl tipWindowCtl = fxmlLoader.getController();
-         location = this.getClass().getResource("/com/os/apps/fileApp/res/tip.png");
-         primaryStage.getIcons().add(new Image(String.valueOf(location)));
-         scene.setFill(Color.TRANSPARENT);
-         primaryStage.initStyle(StageStyle.TRANSPARENT);
-         primaryStage.show();
-         tipWindowCtl.init(primaryStage, this.tipString);
-         primaryStage.setResizable(false);
-      }
-   }
+    public void start(Stage primaryStage) throws IOException {
+        URL location = this.getClass().getResource("/com/os/apps/fileApp/fxmls/TipWindow.fxml");
+        if (location == null) {
+            System.out.println("null");
+        } else {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(location);
+            Parent root = fxmlLoader.load();
+            primaryStage.setTitle("提示");
+            Scene MainScene = new Scene(root);
+            primaryStage.setScene(MainScene);
+            Scene scene = primaryStage.getScene();
+            TipWindowCtl tipWindowCtl = fxmlLoader.getController();
+            location = this.getClass().getResource("/com/os/apps/fileApp/res/tip.png");
+            primaryStage.getIcons().add(new Image(String.valueOf(location)));
+            scene.setFill(Color.TRANSPARENT);
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
+            primaryStage.show();
+            tipWindowCtl.init(primaryStage, this.tipString);
+            primaryStage.setResizable(false);
+        }
+    }
 }
