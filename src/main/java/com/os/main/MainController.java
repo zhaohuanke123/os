449a28712d89befbd3ccdaa42932dfad24d11c04
11828 <1182814995@qq.com>
2023-09-12 18:23:26 +0800

    测试

--- src/main/java/com/os/main/MainController.java
+++ src/main/java/com/os/main/MainController.java
@@ -12,7 +12,6 @@ import com.os.utils.process.ProcessScheduleThread;
 import com.os.utils.ui.StageRecord;
 import com.os.utils.ui.UIThread;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -25,7 +24,6 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -59,7 +57,7 @@ public class MainController {
     @FXML
     private HBox appBox;
     @FXML
-    private Button executableFileButton;
+    private Button systemFileButton;
     @FXML
     private Button processButton;
     @FXML
@@ -88,6 +86,11 @@ public class MainController {
     private Button fileManagerButton;
     boolean isTop = false;
     boolean haveChanged = true;
+    private final String buttonStyle =
+            "-fx-background-color: transparent,aliceblue;" +
+            "-fx-background-radius: 12;" +
+            "-fx-text-fill: black;" +
+            "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 3, 0.0 , 0 , 1 );";
 
     @FXML
     void closeWindow(MouseEvent event) {
@@ -130,14 +133,6 @@ public class MainController {
         this.primaryStage.setIconified(true);
     }
 
-    @FXML
-    void updateTimeButton1(ActionEvent event) {
-    }
-
-    @FXML
-    void updateTimeButton2(ActionEvent event) {
-    }
-
     public void iconInit() {
         this.appWidth = this.scene.getHeight() / 15.0;
         this.sceneWidth = this.scene.getWidth();
@@ -152,10 +147,10 @@ public class MainController {
         this.background.setVisible(true);
         //
 
-        this.executableFileButton.setOnMouseClicked(event -> {
+        this.systemFileButton.setOnMouseClicked(event -> {
             if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 1) {
                 System.out.println("executableFileApp open success");
-                MainController.this.executableFileAppOpen();
+                MainController.this.systemFileAppOpen();
             }
 
         });
@@ -199,33 +194,34 @@ public class MainController {
 
         });
 
-        Button[] buttons = new Button[]{this.executableFileButton,
+        Button[] buttons = new Button[]{this.systemFileButton,
                 this.fileManagerButton, this.processButton,
                 this.occupancyButton, this.helpButton};
 
+        String tooltipStyle = "-fx-font-size: 12";
         Tooltip tooltip = new Tooltip("系统文件表");
-        tooltip.setStyle("-fx-font-size: 12");
-        this.executableFileButton.setTooltip(tooltip);
+        tooltip.setStyle(tooltipStyle);
+        this.systemFileButton.setTooltip(tooltip);
         tooltip = new Tooltip("文件管理器");
-        tooltip.setStyle("-fx-font-size: 12");
+        tooltip.setStyle(tooltipStyle);
         this.fileManagerButton.setTooltip(tooltip);
         tooltip = new Tooltip("进程管理器");
-        tooltip.setStyle("-fx-font-size: 12");
+        tooltip.setStyle(tooltipStyle);
         this.processButton.setTooltip(tooltip);
         tooltip = new Tooltip("占用管理器");
-        tooltip.setStyle("-fx-font-size: 12");
+        tooltip.setStyle(tooltipStyle);
         this.occupancyButton.setTooltip(tooltip);
         tooltip = new Tooltip("帮助");
-        tooltip.setStyle("-fx-font-size: 12");
+        tooltip.setStyle(tooltipStyle);
         this.helpButton.setTooltip(tooltip);
         tooltip = new Tooltip("最小化");
-        tooltip.setStyle("-fx-font-size:12");
+        tooltip.setStyle(tooltipStyle);
         this.minimizeButton.setTooltip(tooltip);
         tooltip = new Tooltip("关闭");
-        tooltip.setStyle("-fx-font-size: 12");
+        tooltip.setStyle(tooltipStyle);
         this.closeButton.setTooltip(tooltip);
         tooltip = new Tooltip("显示桌面");
-        tooltip.setStyle("-fx-font-size: 12");
+        tooltip.setStyle(tooltipStyle);
         this.deskButton.setTooltip(tooltip);
 
         UIThread.mainButtons = buttons;
@@ -303,6 +299,7 @@ public class MainController {
         this.uiThreadInit();
     }
 
+    // 设置按钮的大小
     private void setCompSize(Button button, double width, double height) {
         button.setPrefSize(width, height);
         button.setMinSize(width, height);
@@ -310,6 +307,7 @@ public class MainController {
         button.resize(width, height);
     }
 
+    // 设置图像视图的宽度和高度
     private void setImageViewSize(ImageView button, double width, double height) {
         button.setFitWidth(width);
         button.setFitHeight(height);
@@ -342,8 +340,8 @@ public class MainController {
         this.buttonBarBackGround.setEffect(gaussianBlur);
         // ------------------------------------------------------------
 
-        setCompSize(this.executableFileButton, 1.2 * this.appWidth, 1.2 * this.appWidth);
-        setImageViewSize((ImageView) this.executableFileButton.getGraphic(), this.appWidth * 0.9, this.appWidth * 0.9);
+        setCompSize(this.systemFileButton, 1.2 * this.appWidth, 1.2 * this.appWidth);
+        setImageViewSize((ImageView) this.systemFileButton.getGraphic(), this.appWidth * 0.9, this.appWidth * 0.9);
         setCompSize(this.fileManagerButton, 1.2 * this.appWidth, 1.2 * this.appWidth);
         setImageViewSize((ImageView) this.processButton.getGraphic(), this.appWidth * 0.9, this.appWidth * 0.9);
         setCompSize(this.processButton, 1.2 * this.appWidth, 1.2 * this.appWidth);
@@ -419,7 +417,7 @@ public class MainController {
 
         // 设置文件管理器按钮的下划线效果，并修改其样式
         this.fileManagerButton.setUnderline(true);
-        this.fileManagerButton.setStyle("-fx-background-color: transparent, aliceblue;\n-fx-background-radius: 12;\n-fx-text-fill: black;\n-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 3, 0.0 , 0 , 1 );");
+        this.fileManagerButton.setStyle(buttonStyle);
     }
 
     // 打开帮助界面
@@ -477,125 +475,181 @@ public class MainController {
 
         // 设置帮助界面按钮的下划线效果，并修改其样式
         this.helpButton.setUnderline(true);
-        this.helpButton.setStyle("-fx-background-color: transparent,aliceblue;\n-fx-background-radius:20;\n-fx-text-fill: black;\n-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 3, 0.0 , 0 , 1 );");
+        this.helpButton.setStyle(buttonStyle);
     }
 
+    // 打开设备管理器
     public void occupancyAppOpen() {
-//        Stage stage = checkStage("com/os/apps/occupancyApp");
-//        if (stage != null && !stage.isShowing()) {
-//            removeStage("com/os/apps/occupancyApp");
-//        }
-//
-//        OccupancyApp occupancyApp = new OccupancyApp();
-//        stage = checkStage("com/os/apps/occupancyApp");
-//        if (stage == null) {
-//            try {
-//                stage = new Stage();
-////                double ratioW = this.primaryStage.getWidth() / 1920.0;
-////                double ratioH = this.primaryStage.getHeight() / 1080.0;
-//                stage.setMinHeight(530.0);
-//                stage.setMaxHeight(this.primaryStage.getHeight() - 1.4 * this.appWidth);
-//                stage.setMinWidth(1000.0);
-//                stage.setMaxWidth(this.primaryStage.getWidth());
-//                stage.setWidth(1000.0);
-//                stage.setHeight(530.0);
-//                occupancyApp.start(stage);
-//                stageList.add(new StageRecord("com/os/apps/occupancyApp", stage));
-//            } catch (IOException var7) {
-//                System.out.println(Arrays.toString(var7.getStackTrace()));
-//            }
-//        }
-//
-//        if (stage.isShowing()) {
-//            stage.show();
-//        }
-//
-//        stage.setOnCloseRequest(event -> {
-//            MainController.this.occupancyButton.setUnderline(false);
-//            MainController.this.occupancyButton.setStyle("");
-//        });
-//        stage.setAlwaysOnTop(true);
-//        stage.setIconified(false);
-//        stage.toFront();
-//        updateStageList("com/os/apps/occupancyApp");
-//        this.occupancyButton.setUnderline(true);
-//        this.occupancyButton.setStyle("    -fx-background-color: transparent,     aliceblue;\n    -fx-background-radius: 12;\n    -fx-text-fill: black;\n    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 3, 0.0 , 0 , 1 );");
+        // 窗口名称
+        String stageName = "com/os/apps/occupancyApp";
+
+        // 检查窗口是否已存在
+        Stage stage = checkStage(stageName);
+
+        // 如果窗口存在但未显示，将其移除
+        if (stage != null && !stage.isShowing()) removeStage(stageName);
+
+        // 创建 OccupancyApp 实例
+        OccupancyApp occupancyApp = new OccupancyApp();
+
+        // 再次检查窗口是否存在
+        stage = checkStage(stageName);
+
+        // 如果窗口不存在，则创建新的窗口并添加到 stageList 中
+        if (stage == null) {
+            try {
+                stage = new Stage();
+
+                // 设置窗口的尺寸限制
+                stage.setMinHeight(530.0);
+                stage.setMaxHeight(this.primaryStage.getHeight() - 1.4 * this.appWidth);
+                stage.setMinWidth(1000.0);
+                stage.setMaxWidth(this.primaryStage.getWidth());
+                stage.setHeight(530.0);
+                stage.setWidth(1000.0);
+
+                // 使用 OccupancyApp 实例初始化新窗口
+                occupancyApp.start(stage);
+
+                // 将新窗口记录添加到窗口列表
+                stageList.add(new StageRecord(stageName, stage));
+            } catch (IOException e) {
+                e.getStackTrace();
+            }
+        }
+
+        // 如果窗口已显示，将其显示在最前面
+        if (stage.isShowing()) stage.show();
+
+        // 设置窗口始终位于其他窗口之上
+        stage.setAlwaysOnTop(true);
+        // 将窗口从最小化状态还原
+        stage.setIconified(false);
+        // 将窗口置于最前
+        stage.toFront();
+
+        // 更新窗口列表中的信息
+        updateStageList(stageName);
+
+        // 设置设备管理器按钮的下划线效果，并修改其样式
+        this.occupancyButton.setUnderline(true);
+        this.occupancyButton.setStyle(buttonStyle);
     }
 
+    // 打开进程管理
     public void processAppOpen() {
-//        Stage stage = checkStage("com/os/apps/processApp");
-//        if (stage != null && !stage.isShowing()) {
-//            removeStage("com/os/apps/processApp");
-//        }
-//
-//        ProcessApp processApp = new ProcessApp();
-//        stage = checkStage("com/os/apps/processApp");
-//        if (stage == null) {
-//            try {
-//                stage = new Stage();
-////                double ratioW = this.primaryStage.getWidth() / 1920.0;
-////                double ratioH = this.primaryStage.getHeight() / 1080.0;
-//                stage.setMinHeight(500.0);
-//                stage.setMaxHeight(this.primaryStage.getHeight() - 1.4 * this.appWidth);
-//                stage.setMinWidth(1100.0);
-//                stage.setMaxWidth(this.primaryStage.getWidth());
-//                stage.setWidth(1100.0);
-//                stage.setHeight(500.0);
-//                processApp.start(stage);
-//                stageList.add(new StageRecord("com/os/apps/processApp", stage));
-//            } catch (IOException var7) {
-//                System.out.println(Arrays.toString(var7.getStackTrace()));
-//            }
-//        }
-//
-//        if (stage.isShowing()) {
-//            stage.show();
-//        }
-//
-//        stage.setAlwaysOnTop(true);
-//        stage.setIconified(false);
-//        stage.toFront();
-//        updateStageList("com/os/apps/processApp");
-//        this.processButton.setUnderline(true);
-//        this.processButton.setStyle("    -fx-background-color: transparent,     aliceblue;\n    -fx-background-radius: 12;\n    -fx-text-fill: black;\n    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 3, 0.0 , 0 , 1 );");
+        // 窗口名称
+        String stageName = "com/os/apps/processApp";
+
+        // 检查窗口是否已存在
+        Stage stage = checkStage(stageName);
+
+        // 如果窗口存在但未显示，将其移除
+        if (stage != null && !stage.isShowing()) removeStage(stageName);
+
+        // 创建 ProcessApp 实例
+        ProcessApp processApp = new ProcessApp();
+
+        // 再次检查窗口是否存在
+        stage = checkStage(stageName);
+
+        // 如果窗口不存在，则创建新的窗口并添加到 stageList 中
+        if (stage == null) {
+            try {
+                stage = new Stage();
+
+                // 设置窗口的尺寸限制
+                stage.setMinHeight(500.0);
+                stage.setMaxHeight(this.primaryStage.getHeight() - 1.4 * this.appWidth);
+                stage.setMinWidth(1100.0);
+                stage.setMaxWidth(this.primaryStage.getWidth());
+                stage.setHeight(500.0);
+                stage.setWidth(1100.0);
+
+                // 使用 ProcessApp 实例初始化新窗口
+                processApp.start(stage);
+
+                // 将新窗口记录添加到窗口列表
+                stageList.add(new StageRecord(stageName, stage));
+            } catch (IOException e) {
+                e.getStackTrace();
+            }
+        }
+
+        // 如果窗口已显示，将其显示在最前面
+        if (stage.isShowing()) stage.show();
+
+        // 设置窗口始终位于其他窗口之上
+        stage.setAlwaysOnTop(true);
+        // 将窗口从最小化状态还原
+        stage.setIconified(false);
+        // 将窗口置于最前
+        stage.toFront();
+
+        // 更新窗口列表中的信息
+        updateStageList(stageName);
+
+        // 设置设备管理器按钮的下划线效果，并修改其样式
+        this.processButton.setUnderline(true);
+        this.processButton.setStyle(buttonStyle);
     }
 
-    public void executableFileAppOpen() {
-//        Stage executableFileStage = checkStage("com/os/apps/systemFileApp");
-//        if (executableFileStage != null && !executableFileStage.isShowing()) {
-//            removeStage("com/os/apps/systemFileApp");
-//        }
-//
-//        SystemFileApp systemFileApp = new SystemFileApp();
-//        Stage stage = checkStage("com/os/apps/systemFileApp");
-//        if (stage == null) {
-//            try {
-//                stage = new Stage();
-////                double ratioW = this.primaryStage.getWidth() / 1920.0;
-////                double ratioH = this.primaryStage.getHeight() / 1080.0;
-//                stage.setMinHeight(500.0);
-//                stage.setMaxHeight(this.primaryStage.getHeight() - 1.4 * this.appWidth);
-//                stage.setMinWidth(500.0);
-//                stage.setMaxWidth(this.primaryStage.getWidth());
-//                stage.setWidth(500.0);
-//                stage.setHeight(500.0);
-//                systemFileApp.start(stage);
-//                stageList.add(new StageRecord("com/os/apps/systemFileApp", stage));
-//            } catch (IOException var8) {
-//                System.out.println(Arrays.toString(var8.getStackTrace()));
-//            }
-//        }
-//
-//        if (stage.isShowing()) {
-//            stage.show();
-//        }
-//
-//        stage.setAlwaysOnTop(true);
-//        stage.setIconified(false);
-//        stage.toFront();
-//        updateStageList("com/os/apps/systemFileApp");
-//        this.executableFileButton.setUnderline(true);
-//        this.executableFileButton.setStyle("    -fx-background-color: transparent,     aliceblue;\n    -fx-background-radius: 12;\n    -fx-text-fill: black;\n    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 3, 0.0 , 0 , 1 );");
+    // 打开系统文件表
+    public void systemFileAppOpen() {
+        // 窗口名称
+        String stageName = "com/os/apps/systemFileApp";
+
+        // 检查窗口是否已存在
+        Stage executableFileStage = checkStage(stageName);
+
+        // 如果窗口存在但未显示，将其移除
+        if (executableFileStage != null && !executableFileStage.isShowing()) removeStage(stageName);
+
+        // 创建 SystemFileApp 实例
+        SystemFileApp systemFileApp = new SystemFileApp();
+
+        // 再次检查窗口是否存在
+        Stage stage = checkStage(stageName);
+
+        // 如果窗口不存在，则创建新的窗口并添加到 stageList 中
+        if (stage == null) {
+            try {
+                stage = new Stage();
+
+                // 设置窗口的尺寸限制
+                stage.setMinHeight(500.0);
+                stage.setMaxHeight(this.primaryStage.getHeight() - 1.4 * this.appWidth);
+                stage.setMinWidth(500.0);
+                stage.setMaxWidth(this.primaryStage.getWidth());
+                stage.setHeight(500.0);
+                stage.setWidth(500.0);
+
+                // 使用 SystemFileApp 实例初始化新窗口
+                systemFileApp.start(stage);
+
+                // 将新窗口记录添加到窗口列表
+                stageList.add(new StageRecord(stageName, stage));
+            } catch (IOException e) {
+                e.getStackTrace();
+            }
+        }
+
+        // 如果窗口已显示，将其显示在最前面
+        if (stage.isShowing()) stage.show();
+
+        // 设置窗口始终位于其他窗口之上
+        stage.setAlwaysOnTop(true);
+        // 将窗口从最小化状态还原
+        stage.setIconified(false);
+        // 将窗口置于最前
+        stage.toFront();
+
+        // 更新窗口列表中的信息
+        updateStageList(stageName);
+
+        // 设置设备管理器按钮的下划线效果，并修改其样式
+        this.systemFileButton.setUnderline(true);
+        this.systemFileButton.setStyle(buttonStyle);
     }
 
     // 检查窗口是否已存在
@@ -632,22 +686,31 @@ public class MainController {
         }
     }
 
+    // 初始化时间显示
     public void timeInit() {
+        // 获取当前日期和时间
         Date date = new Date();
+
+        // 格式化日期和时间
         String hour = String.format("%tH", date);
         String minute = String.format("%tM", date);
         String second = String.format("%tS", date);
         String year = String.format("%ty", date);
         String month = String.format("%tm", date);
         String day = String.format("%td", date);
+
+        // 设置时间按钮的最小和最大宽度
         this.timeButton1.setMinWidth(2 * this.appWidth);
         this.timeButton1.setMaxWidth(2 * this.appWidth);
         this.timeButton2.setMinWidth(2 * this.appWidth);
         this.timeButton2.setMaxWidth(2 * this.appWidth);
+
+        // 设置时间按钮的文本格式
         this.timeButton1.setText(hour + ":" + minute + ":" + second);
         this.timeButton2.setText("20" + year + "/" + month + "/" + day);
     }
 
+    // 初始化ui线程
     public void uiThreadInit() {
         UIThread.timeButton1 = this.timeButton1;
         UIThread.timeButton2 = this.timeButton2;
@@ -655,6 +718,7 @@ public class MainController {
         this.uiThread.start();
     }
 
+    // 初始化进程线程
     public void processThreadInit() {
         ProcessManager.init();
         this.processScheduleThread.Init();
