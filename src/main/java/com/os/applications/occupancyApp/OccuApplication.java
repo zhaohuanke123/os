a543b19eafc91435726eeb1e3c81523dfee79937
JoVanko <49305713+zhaohuanke123@users.noreply.github.com>
2023-10-09 22:04:03 +0800

    开始增加折线图

---
+++ src/main/java/com/os/applications/occupancyApp/OccuApplication.java
@@ -0,0 +1,9 @@
+package com.os.applications.occupancyApp;
+
+import com.os.applications.BaseApp;
+
+public class OccuApplication extends BaseApp<OccuApplicationController> {
+    public OccuApplication(String fxmlPath, String IconPath, String TitleName, double sceneWidth, double sceneHeight) {
+        super(fxmlPath, IconPath, TitleName, sceneWidth, sceneHeight);
+    }
+}
---
+++ src/main/java/com/os/applications/occupancyApp/OccuApplicationController.java
@@ -0,0 +1,6 @@
+package com.os.applications.occupancyApp;
+
+import com.os.applications.BaseController;
+
+public class OccuApplicationController extends BaseController {
+}
