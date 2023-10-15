ed26798ea532794bd329c89b275434c8b5c86177
zhengcb21 <zhengcb21@outlook.com>
2023-10-15 17:20:23 +0800

    进程管理修改

---
+++ src/main/java/com/os/applications/helpApp/HelpAppController.java
@@ -0,0 +1,16 @@
+package com.os.applications.helpApp;
+
+import com.os.applications.BaseController;
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+
+public class HelpAppController extends BaseController{
+    @FXML
+    private TabPane tabPane;
+    @Override
+    public void init(Stage stage) {
+        super.init(stage);
+    }
+
+}
