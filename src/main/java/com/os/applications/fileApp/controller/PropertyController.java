f39ba5d34f2151b83817f922218747b8d7a5e3b3
11828 <1182814995@qq.com>
2023-10-28 22:39:58 +0800

    修改布局

--- src/main/java/com/os/applications/fileApp/controller/PropertyController.java
+++ src/main/java/com/os/applications/fileApp/controller/PropertyController.java
@@ -1,10 +1,15 @@
 package com.os.applications.fileApp.controller;
 
+import com.os.applications.processControlApp.processSystem.ProcessManager;
+import com.os.utility.uiUtil.DrawUtil;
+import javafx.application.Platform;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class PropertyController extends BaseFileController {
    public Label title;
@@ -23,4 +28,18 @@ public class PropertyController extends BaseFileController {
    public Label propertyField;
    public Label propertyLabel;
    public Label propertyLabel1;
+
+   @Override
+   public void init(Stage stage) {
+      super.init(stage);
+
+      // 创建绘图工具
+      DrawUtil drawUtil = new DrawUtil();
+      drawUtil.addDrawFunc(stage, this.topMainPane);
+
+      // 监听窗口大小变化，根据窗口大小自适应布局
+      stage.widthProperty().addListener((observable, oldValue, newValue) -> Platform.runLater(this::adaptWindow));
+      stage.heightProperty().addListener((observable, oldValue, newValue) -> Platform.runLater(this::adaptWindow));
+      this.adaptWindow();  // 初始时适应窗口
+   }
 }
